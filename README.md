1c2a112105bbd7d83c113638a0f1af2ccf50a490
Peter J Langley <langleyjpeter@gmail.com>
2014-03-24 20:56:01 +0000

    Simple JavaScript Countdown Timer

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript Countdown Timer
+
+Simple countdown timer - allows you to specify any time in the future to begin counting down from.
+
+See more here; [http://www.codechewing.com/library/javascript-countdown-timer/](http://www.codechewing.com/library/javascript-countdown-timer/)
---
+++ countdownTimer.js
@@ -0,0 +1,104 @@
+var startTime = new Date(),
+    expiryTime = new Date(),
+    hourElem = document.getElementById('js-hour'),
+    minuteElem = document.getElementById('js-minute'),
+    secondElem = document.getElementById('js-second');
+
+// Set up expiry time
+expiryTime.setHours( expiryTime.getHours() + 1 );
+expiryTime.setMinutes( expiryTime.getMinutes() + 5 );
+expiryTime.setSeconds( expiryTime.getSeconds() + 10 );
+
+var diffInMs = expiryTime - startTime,
+    diffInSecs = Math.round( diffInMs / 1000 ),
+    amountOfHours = Math.floor( diffInSecs / 3600 ),
+    amountOfSeconds = diffInSecs - (amountOfHours * 3600),
+    amountOfMinutes = Math.floor( amountOfSeconds / 60 ),
+    amountOfSeconds = amountOfSeconds - ( amountOfMinutes * 60 );
+
+// Set up the countdown timer for display
+// Set up the hours
+if( amountOfHours > 0 ) {
+  hourElem.innerHTML = (amountOfHours < 10)
+  ? '0' + amountOfHours + ' : '
+  : amountOfHours + ' : ';
+} else {
+  hourElem.innerHTML = '00 : ';
+}
+
+// Set up the minutes
+if( amountOfMinutes > 0 ) {
+  minuteElem.innerHTML = ( amountOfMinutes < 10 )
+  ? '0' + amountOfMinutes + ' : '
+  : amountOfMinutes + ' : ';
+} else {
+  minuteElem.innerHTML = '00 : ';
+}
+
+// // Set up the seconds
+if( amountOfSeconds > 0 ) {
+  secondElem.innerHTML = (amountOfSeconds < 10)
+  ? '0' + amountOfSeconds
+  : amountOfSeconds;
+} else {
+  secondElem.innerHTML = '00';
+}
+
+function countDown() {
+  var dateNow = new Date();
+ 
+  // If we're not at the end of the timer, continue the countdown
+  if( expiryTime > dateNow ) {
+ 
+  // References to current countdown values
+  var hours = parseInt(hourElem.innerHTML);
+      minutes = parseInt(minuteElem.innerHTML),
+      seconds = parseInt(secondElem.innerHTML);
+ 
+  // Update the hour if necessary
+  if( minutes == 0 && seconds == 0) {
+    --hours;
+ 
+    hourElem.innerHTML = ( hours < 10 ) ? '0' + (hours) + ' : ' : (hours) + ' : ';
+    minuteElem.innerHTML = '59 : ';
+    secondElem.innerHTML = '59';
+    return;
+  }
+ 
+  // Update the minute if necessary
+  if( seconds == 0 ) {
+ 
+    if( minutes > 0 ) {
+      --minutes;
+      minuteElem.innerHTML = ( minutes > 10 ) ? minutes + ' : ' : '0' + minutes + ' : ';
+ 
+      } else {
+        minuteElem.innerHTML = '59' + ' : ';
+      }
+ 
+      return secondElem.innerHTML = '59';
+ 
+    } else {
+      --seconds;
+      secondElem.innerHTML = ( seconds < 10 ) ? '0' + seconds : seconds;
+    }
+ 
+  } else {
+    // Reset the seconds
+    secondElem.innerHTML = '00';
+ 
+    // Clear interval and fire countDownOnComplete()
+    clearInterval(countDownInterval);
+    countDownOnComplete();
+  }
+}
+
+function countDownOnComplete() {
+  console.log('Countdown timer has completed!');
+}
+
+window.onload = function() {
+  // Begin the countdown!
+  countDownInterval = setInterval( countDown, 1000 );
+}
+
